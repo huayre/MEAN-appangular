cc20ca9781aa6ee66b183be300e81be8f2643638
Roy carlos Huayre Bujaico <roy.huayre@unas.edu.pe>
2020-10-10 23:59:01 -0500

    commit modules app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,34 +1,24 @@
+import { AuthRoutingModule } from './auth/auth.routing';
+import { PagesRoutingModule } from './pages/pages.routing';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 
 
-const routes:Routes=[
-   {path:'',component:PagesComponent,
-    children:[
-      {path:'progress',component:ProgressComponent},
-      {path:'grafica1',component:Grafica1Component},
-      {path:'dashboard',component:DashboardComponent},       
-      {path:'',redirectTo:'/dashboard',pathMatch:'full'},
-    ]},
-  
-   {path:'login',component:LoginComponent},
-   {path:'register',component:RegisterComponent}, 
+
+const routes:Routes=[ 
+   {path:'',redirectTo:'/dashboard',pathMatch:'full'},
    {path:'**',component:NopagefoundComponent},
 ];
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports : [RouterModule]
 })
